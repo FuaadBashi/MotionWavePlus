fb099a6cd509dbc2e7aed8f1922640127ea3a8af
Fuaad Shurie <fbshurie@hotmail.com>
2026-05-12 15:45:10 +0300

    final fix

--- src/audio/AudioData.h
+++ src/audio/AudioData.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "../../include/miniaudio.h"
-#include <SDL2/SDL.h>
 #include <mutex>
 
 
 struct AudioData {
-    Uint8* buf;  // pointer to the audio file in memory
-    Uint32 len;   // ttal size of the audio file in bytes
-    Uint32 pos;   // Current playback position (g eg a bookmark)
     float audio_samples[4096];
     int sample_count; 
     std::mutex audio_mutex;
